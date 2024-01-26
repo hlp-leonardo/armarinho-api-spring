9a1f09a6686a4466cc263b3aff2b3cf32fe769d6
Hugo Pereira <hleonardo@outlook.com>
2024-01-26 11:33:31 +0000

    update product-type

--- src/main/java/com/armarinho/armarinho/api/controllers/ProductTypeController.java
+++ src/main/java/com/armarinho/armarinho/api/controllers/ProductTypeController.java
@@ -1,5 +1,6 @@
 package com.armarinho.armarinho.api.controllers;
 
+import com.armarinho.armarinho.api.dtos.ProductTypeDTO;
 import com.armarinho.armarinho.api.models.ProductType;
 import com.armarinho.armarinho.api.services.ProductTypeService;
 import org.springframework.web.bind.annotation.*;
@@ -16,27 +17,27 @@ public class ProductTypeController {
     }
 
     @PostMapping
-    public ProductType create(@RequestBody ProductType productType) {
-        productType = service.create(productType);
-        return productType;
+    public ProductTypeDTO create(@RequestBody ProductType productType) {
+        ProductTypeDTO createProductType = service.create(productType);
+        return createProductType;
     }
 
     @GetMapping
-    public List<ProductType> getAll() {
-        List<ProductType> allProductTypes = service.getAll();
+    public List<ProductTypeDTO> getAll() {
+        List<ProductTypeDTO> allProductTypes = service.getAll();
         return allProductTypes;
     }
 
     @GetMapping("/{id}")
-    public ProductType getOne(@PathVariable("id") int id) {
-        ProductType productType = service.getOne(id);
+    public ProductTypeDTO getOne(@PathVariable("id") int id) {
+        ProductTypeDTO productType = service.getOne(id);
         return productType;
     }
 
     @PutMapping("/{id}")
-    public ProductType update(@PathVariable("id") int id, @RequestBody ProductType productType) {
-        productType = service.update(id, productType);
-        return productType;
+    public ProductTypeDTO update(@PathVariable("id") int id, @RequestBody ProductType productType) {
+        ProductTypeDTO productTypeDTO = service.update(id, productType);
+        return productTypeDTO;
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/armarinho/armarinho/api/services/ProductTypeService.java
+++ src/main/java/com/armarinho/armarinho/api/services/ProductTypeService.java
@@ -1,9 +1,11 @@
 package com.armarinho.armarinho.api.services;
 
+import com.armarinho.armarinho.api.dtos.ProductTypeDTO;
 import com.armarinho.armarinho.api.models.ProductType;
 import com.armarinho.armarinho.api.repository.ProductTypeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,32 +16,56 @@ public class ProductTypeService {
         this.repository = repository;
     }
 
-    public ProductType create(ProductType productType) {
+    private ProductTypeDTO convertToProductTypeDTO(ProductType productType) {
+        ProductTypeDTO productTypeDTO = new ProductTypeDTO();
+        productTypeDTO.setId(productType.getId());
+        productTypeDTO.setName(productType.getName());
+        return productTypeDTO;
+    }
+
+    private List<ProductTypeDTO> convertListToProductTypeDTO(List<ProductType> productTypeList) {
+        List<ProductTypeDTO> allProductTypesDTO = new ArrayList<>();
+        for (int i=0; i<productTypeList.size(); i++) {
+            ProductType existingProductType = productTypeList.get(i);
+            if (existingProductType != null) {
+                ProductTypeDTO productTypeDTO = new ProductTypeDTO();
+                productTypeDTO.setId(existingProductType.getId());
+                productTypeDTO.setName(existingProductType.getName());
+                allProductTypesDTO.add(productTypeDTO);
+            }
+        }
+        return allProductTypesDTO;
+    }
+
+    public ProductTypeDTO create(ProductType productType) {
         productType = repository.save(productType);
-        return productType;
+        ProductTypeDTO productTypeDTO = convertToProductTypeDTO(productType);
+        return productTypeDTO;
     }
 
-    public List<ProductType> getAll() {
+    public List<ProductTypeDTO> getAll() {
         List<ProductType> allProductTypes = repository.findAll();
-        return allProductTypes;
+        List<ProductTypeDTO> allProductTypesDTO = convertListToProductTypeDTO(allProductTypes);
+        return allProductTypesDTO;
     }
 
-    public ProductType getOne(int id) {
+    public ProductTypeDTO getOne(int id) {
         Optional<ProductType> productType = repository.findById(id);
         if (productType.isPresent()) {
-            return productType.get();
+            ProductTypeDTO productTypeDTO = convertToProductTypeDTO(productType.get());
+            return productTypeDTO;
         } else {
             return null;
         }
     }
 
-    public ProductType update(int id, ProductType productType) {
+    public ProductTypeDTO update(int id, ProductType productType) {
         Optional<ProductType> existingProductType = repository.findById(id);
         if (existingProductType.isPresent()) {
-            productType.setId(existingProductType.get().getId());
-            productType.setName(existingProductType.get().getName());
-            productType = repository.save(productType);
-            return productType;
+            existingProductType.get().setName(productType.getName());
+            repository.save(existingProductType.get());
+            ProductTypeDTO productTypeDTO = convertToProductTypeDTO(existingProductType.get());
+            return productTypeDTO;
         } else  {
             return null;
         }
